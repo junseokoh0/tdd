99599dc68a40c0e73fcca66e09c31840680f0bbf
junseokoh0 <junseok@bodycodi.com>
2022-05-22 22:26:57 +0900

    times()에 있는 5를 생성자로 이동
    5는 생성자의 파라미터에서 넘어왔기 때문에 이동할 수 있다

--- src/main/java/personal/junseok/tdd/Dollar.java
+++ src/main/java/personal/junseok/tdd/Dollar.java
@@ -5,9 +5,10 @@ public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
-        this.amount = 2 * 5;
+        this.amount = this.amount * 2;
     }
 }

d4079ac7c42b26ccf1c628e7121d4aadcfe40607
junseokoh0 <junseok@bodycodi.com>
2022-06-06 15:12:37 +0900

    Money에 Dollar를 반환하는 팩토리 메서드 도입

--- src/main/java/personal/junseok/tdd/Money.java
+++ src/main/java/personal/junseok/tdd/Money.java
@@ -3,6 +3,10 @@ package personal.junseok.tdd;
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;

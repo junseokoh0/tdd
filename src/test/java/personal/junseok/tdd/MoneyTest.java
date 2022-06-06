c6e9ff2f558c77107d806ddf24d3e9bfbfe9e5ea
junseokoh0 <junseok@bodycodi.com>
2022-06-06 14:57:35 +0900

    Franc과 Dollar를 비교하는 테스트 코드 작성

--- src/test/java/personal/junseok/tdd/MoneyTest.java
+++ src/test/java/personal/junseok/tdd/MoneyTest.java
@@ -18,8 +18,9 @@ class MoneyTest {
     void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
-        assertEquals(new Franc(5), new Franc(5));
+        assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Franc(5), new Franc(6));
+        assertNotEquals(new Franc(5), new Dollar(5));
     }
 
     @Test

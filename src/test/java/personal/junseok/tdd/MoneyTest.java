87a1661e67aeb7ae9f652ed72643b0347e828d3f
junseokoh0 <junseok@bodycodi.com>
2022-06-12 21:22:59 +0900

    testFrancMultiplication() 메서드 삭제

--- src/test/java/personal/junseok/tdd/MoneyTest.java
+++ src/test/java/personal/junseok/tdd/MoneyTest.java
@@ -21,13 +21,6 @@ class MoneyTest {
     }
 
     @Test
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
